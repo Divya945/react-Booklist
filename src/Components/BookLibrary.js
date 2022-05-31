0837cf9321f353bac5c7c232d63e592159caaa01
Divya945 <divyadvs945@gmail.com>
2022-05-31 15:44:33 +0530

    1 file modified

--- src/Components/BookLibrary.js
+++ src/Components/BookLibrary.js
@@ -1,47 +1,39 @@
 import React from 'react';
 
+const firstbook = {
+  img: 'https://m.media-amazon.com/images/I/71KKZlVjbwL._AC_UL320_.jpg',
+  title: 'An Autobiography of Abdul Kalam',
+  autor: ' by Arun Tiwari and A. P. J. Abdul Kalam',
+};
+const secondimg = {
+  img: 'https://m.media-amazon.com/images/I/51-noH62XpL._AC_UL320_.jpg',
+  title: 'The Power of Your Subconscious Mind',
+  author: 'by Joseph Murphy',
+};
 export default function BookLibrary() {
   return (
     <section className="booklist">
-      <Books />
-      <Books />
-      <Books />
-      <Books />
-      <Books />
-      <Books />
-      <Books />
-      <Books />
-      <Books />
-      <Books />
+      <Books
+        img={firstbook.img}
+        author={firstbook.autor}
+        title={firstbook.title}
+      ></Books>
+      <Books
+        img={secondimg.img}
+        author={secondimg.autor}
+        title={secondimg.title}
+      />
     </section>
   );
 }
 
-const Books = () => {
+const Books = (props) => {
+  const { img, author, title } = props;
   return (
     <articel className="bookPanel">
-      <Image />
-      <Title />
-      <Name />
+      <img src={props.img} alt="" />
+      <h3>{props.author}</h3>
+      <p>{props.title}</p>
     </articel>
-  );
-};
-
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/71KKZlVjbwL._AC_UL320_.jpg"
-      alt="1"
-    />
-  );
-};
-const Title = () => {
-  return <h3>An Autobiography of Abdul Kalam</h3>;
-};
-const Name = () => {
-  return (
-    <h5 style={{ color: '#c9c9c9' }}>
-      by Arun Tiwari and A. P. J. Abdul Kalam
-    </h5>
   );
 };
