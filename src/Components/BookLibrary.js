bf5bed6f71284e2a5025abe4dc8e28872991fa2c
Divya945 <divyadvs945@gmail.com>
2022-05-31 16:23:27 +0530

    1 file modified

--- src/Components/BookLibrary.js
+++ src/Components/BookLibrary.js
@@ -1,39 +1,38 @@
 import React from 'react';
 
-const firstbook = {
-  img: 'https://m.media-amazon.com/images/I/71KKZlVjbwL._AC_UL320_.jpg',
-  title: 'An Autobiography of Abdul Kalam',
-  autor: ' by Arun Tiwari and A. P. J. Abdul Kalam',
-};
-const secondimg = {
-  img: 'https://m.media-amazon.com/images/I/51-noH62XpL._AC_UL320_.jpg',
-  title: 'The Power of Your Subconscious Mind',
-  author: 'by Joseph Murphy',
-};
+const books = [
+  {
+    img: 'https://m.media-amazon.com/images/I/71KKZlVjbwL._AC_UL320_.jpg',
+    title: 'An Autobiography of Abdul Kalam',
+    author: 'by Arun Tiwari and A. P. J. Abdul Kalam',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/51-noH62XpL._AC_UL320_.jpg',
+    title: 'The Power of Your Subconscious Mind',
+    author: 'by Joseph Murphy',
+  },
+];
+
+// const names = ['person1', 'person2', 'person3'];
+// const newnmaes = names.map((name)=>{return <h1>{name}</h1>})
 export default function BookLibrary() {
   return (
     <section className="booklist">
-      <Books
-        img={firstbook.img}
-        author={firstbook.autor}
-        title={firstbook.title}
-      ></Books>
-      <Books
-        img={secondimg.img}
-        author={secondimg.autor}
-        title={secondimg.title}
-      />
-    </section> 
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Books book={book}></Books>;
+      })}
+    </section>
   );
 }
 
 const Books = (props) => {
-  const { img, author, title } = props;
+  const { img, title, author } = props.book;
   return (
     <articel className="bookPanel">
-      <img src={props.img} alt="" />
-      <h3>{props.author}</h3>
-      <p>{props.title}</p>
+      <img src={img} alt="" />
+      <h3>{title}</h3>
+      <p>{author} </p>
     </articel>
   );
 };
